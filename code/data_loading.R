2525685f3d78c21966c2434503371ba729b0a612
nriosvillaco <nicholasrios1213@gmail.com>
2023-12-10 18:24:27 -0500

    add code to load test data

---
+++ code/data_loading.R
@@ -0,0 +1,14 @@
+#install.packages("RSocrata")
+
+library(RSocrata)
+library(here)
+
+df <- read.socrata(
+  "https://data.usaid.gov/resource/azij-hu6e.json?$limit=25", #limit to first 25 rows
+  app_token = "YOURAPPTOKENHERE",
+)
+
+#save data
+
+filepath <- here("data/raw")
+save(df, file = paste0(filepath, "/", "raw_data.RData"))
